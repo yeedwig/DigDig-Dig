c53820f4f2b5fed46e0fd7ac3475871f445d9e6d
201924496 <moonlight6750@naver.com>
2024-02-16 23:21:30 +0900

    땅 파는거

--- Assets/TileMaps/GroundPrefabs/Ground.cs
+++ Assets/TileMaps/GroundPrefabs/Ground.cs
@@ -5,30 +5,29 @@ using UnityEngine.Tilemaps;
 
 public class Ground : MonoBehaviour
 {
-    public float health;
+    public float currentHealth,maxHealth,startBreakingHealth,almostBrokenHealth;
     public int groundLevel;
     public Tilemap groundTileMap;
     public Sprite[] groundSprites;
     SpriteRenderer sr;
+    BoxCollider2D bc;
     public int x, y;
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        bc = GetComponent<BoxCollider2D>();
         SelectGroundLevelHealthSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (health < 0)
-        {
-            this.gameObject.SetActive(false);
-        }
+        ChangeSpriteByCurrentHealth();
     }
     public void takeDamage(float damage)
     {
-        health -= damage;
+        currentHealth -= damage;
     }
 
     public void SelectGroundLevelHealthSprite()
@@ -38,18 +37,38 @@ public class Ground : MonoBehaviour
         if(groundGridPosition.y>-5)
         {
             groundLevel = 1;
-            health = 300.0f;
+            maxHealth = 200.0f;
         }
         else if(groundGridPosition.y>-7&&groundGridPosition.y<=-5)
         {
             groundLevel = 2;
-            health = 500.0f;
+            maxHealth = 500.0f;
         }
         else
         {
             groundLevel = 3;
-            health = 1000.0f;
+            maxHealth = 1000.0f;
         }
-        sr.sprite = groundSprites[groundLevel-1];
+        currentHealth = maxHealth;
+        startBreakingHealth = maxHealth * 0.7f;
+        almostBrokenHealth = maxHealth * 0.3f;
+        sr.sprite = groundSprites[((groundLevel-1)*3)];
+    }
+
+    public void ChangeSpriteByCurrentHealth()
+    {
+        if(currentHealth < 0)
+        {
+            sr.sprite = null;
+            bc.enabled = false;
+        }
+        else if(currentHealth <almostBrokenHealth)
+        {
+            sr.sprite = groundSprites[((groundLevel - 1) * 3) + 2];
+        }
+        else if (currentHealth < startBreakingHealth)
+        {
+            sr.sprite = groundSprites[((groundLevel - 1) * 3)+1];
+        }
     }
 }
