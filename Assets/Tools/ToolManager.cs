db462ac53ad8dcb83126a797428ea1a59e8c8ffb
yeedwig <69492855+yeedwig@users.noreply.github.com>
2024-03-08 15:53:05 +0900

    ToolsTesting

--- Assets/Tools/ToolManager.cs
+++ Assets/Tools/ToolManager.cs
@@ -67,6 +67,10 @@ public class ToolManager : MonoBehaviour
 
             sp.sprite = shovelSkins[skinNr].sprites[spriteNr];
         }
+        else
+        {
+            
+        }
         //if Drill
     }
 
