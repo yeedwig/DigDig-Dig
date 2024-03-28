daa8e281677296b94839aa8655c77df60da86a14
yeedwig <69492855+yeedwig@users.noreply.github.com>
2024-03-28 15:50:25 +0900

    인벤토리 Trash 부분 완성

--- Assets/Inventory/TrashSlot.cs
+++ Assets/Inventory/TrashSlot.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class TrashSlot : MonoBehaviour
+public class TrashSlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag.GetComponent<InventoryItem>().item != null)
         {
+            
             Debug.Log("Trash");
             InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
 
-            inventoryItem.parentAfterDrag = transform;
+            Destroy(inventoryItem.gameObject);
 
-            Destroy(this.GetComponentInChildren<InventoryItem>());
         }
         else
             Debug.Log("Not in Loop!");
