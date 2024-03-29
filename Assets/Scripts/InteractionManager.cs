b6fc47d4761fb237edbdec97f6fb60f409936cf0
123minjae <103908780+123minjae@users.noreply.github.com>
2024-03-29 18:49:08 +0900

    엘리베이터 구현 중

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class InteractionManager : MonoBehaviour
 {
     [SerializeField] GameObject structureCheckPos;
+    [SerializeField] GameObject elevatorCheckPos;
     private int layerMask;
     private bool isOnRail;
     [SerializeField] GameObject elevator;
+    private Rigidbody2D elevatorRB;
+    private bool isOnElevator;
     // Start is called before the first frame update
     void Start()
     {
         layerMask = 1 << LayerMask.NameToLayer("Structure");
+        elevatorRB = elevator.GetComponent<Rigidbody2D>();
         isOnRail = false;
+        isOnElevator = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Collider2D structure = Physics2D.OverlapCircle(structureCheckPos.transform.position, 0.4f, layerMask);
-        if(structure != null)
+        Collider2D elevatorCheck = Physics2D.OverlapCircle(elevatorCheckPos.transform.position, 0.1f, layerMask);
+        if (structure != null)
         {
             if (structure.gameObject.tag == "Rail" && Input.GetKeyDown(KeyCode.F))
             {
@@ -33,10 +39,6 @@ public class InteractionManager : MonoBehaviour
                 }
                 isOnRail = !isOnRail;
             }
-            if(structure.gameObject.tag == "Elevator" && Input.GetKeyDown(KeyCode.F))
-            {
-                elevator.transform.position = structure.transform.position;
-            }
         }
         else
         {
@@ -46,7 +48,37 @@ public class InteractionManager : MonoBehaviour
                 isOnRail = false;
             }
         }
+
+        if(elevatorCheck != null)
+        {
+            if(elevatorCheck.gameObject.tag == "Elevator" && Input.GetKeyDown(KeyCode.F))
+            {
+                if (!isOnElevator)
+                {
+                    isOnElevator = true;
+                    elevator.SetActive(true);
+                    elevator.transform.position = elevatorCheck.transform.position;
+                    //StartCoroutine(MoveElevator());
+                }
+                
+            }
+        }
         
+    }
+    IEnumerator MoveElevator()
+    {
+        float elevatorSpeed = 0.0f;
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            Debug.Log(elevatorSpeed);
+            elevatorRB.velocity = Vector2.down * (elevatorSpeed);
+            if (elevatorSpeed < 3.0f)
+            {
+                elevatorSpeed += 0.5f;
+            }
+        }
+        yield return null;
     }
 
     private void OnDrawGizmos()
