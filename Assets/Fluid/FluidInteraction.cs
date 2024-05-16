67d9201b3acaf907887f2096f6a74edcc3207c54
123minjae <moonlight6750@naver.com>
2024-05-16 22:05:20 +0900

    용암 데미지 구현 완료

--- Assets/Fluid/FluidInteraction.cs
+++ Assets/Fluid/FluidInteraction.cs
@@ -10,7 +10,17 @@ public class FluidInteraction : MonoBehaviour
     private float lavaDamageTimer;
     [SerializeField] float lavaDamageTimerGap;
     [SerializeField] float lavaDamage;
+    [SerializeField] int lavaDamageNumMax;
+    private int lavaDamageNum;
 
+    //Water Damage
+    int waterMask;
+    Collider2D waterCollider;
+    [SerializeField] float breathMax;
+    private float breath;
+    private float waterDamageTimer;
+    [SerializeField] float waterDamageTimerGap;
+    [SerializeField] float waterDamage;
 
 
     [SerializeField] GameObject leftTop, rightBottom;
@@ -20,29 +30,54 @@ public class FluidInteraction : MonoBehaviour
     {
         health = this.GetComponent<Health>();
         lavaMask = LayerMask.GetMask("Lava");
+        waterMask = LayerMask.GetMask("Water");
     }
 
     // Update is called once per frame
     void Update()
     {
         GiveLavaDamage();
+        GiveWaterDamage();
     }
+
+    //용암에 접촉하면 lavaDamageTotalTimer가 초기화
+    //해당 변수가 0이 되기 전까지 계속 데미지를 받음
 
     private void GiveLavaDamage()
     {
         lavaCollider = Physics2D.OverlapArea(leftTop.transform.position, rightBottom.transform.position, lavaMask);
+        //용암에 접촉하고 있는지 확인
         if(lavaCollider != null)
         {
             lavaDamageTimer -= Time.deltaTime;
+            lavaDamageNum = lavaDamageNumMax;
         }
         else
         {
-            lavaDamageTimer = 0.1f;
+            if(lavaDamageNum>0)
+            {
+                lavaDamageTimer -= Time.deltaTime;
+            }
+            else
+            {
+                lavaDamageTimer = 0.1f;
+            }
         }
+
         if(lavaDamageTimer < 0)
         {
             health.takeDamage(lavaDamage);
+            lavaDamageNum--;
             lavaDamageTimer = lavaDamageTimerGap;
+        }
+    }
+
+    private void GiveWaterDamage()
+    {
+        waterCollider = Physics2D.OverlapArea(leftTop.transform.position, rightBottom.transform.position, waterMask);
+        if(waterCollider != null)
+        {
+            
         }
     }
 
