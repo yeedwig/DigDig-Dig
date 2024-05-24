a35a9316c65a8db959f5b204c68fdfc2c551cacf
yeedwig <69492855+yeedwig@users.noreply.github.com>
2024-05-24 20:32:17 +0900

    making moleDoor

--- Assets/Ground/Brushes/MoleCity/MoleDoor.cs
+++ Assets/Ground/Brushes/MoleCity/MoleDoor.cs
@@ -4,44 +4,19 @@ using UnityEngine;
 
 public class MoleDoor : MonoBehaviour
 {
-    private GameManager gameManager;
-    private bool doorOpened;
+    public bool doorOpened;
     private Animator anim;
-    [SerializeField] private GameObject moleDoor;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        gameManager = FindFirstObjectByType<GameManager>();
-        doorOpened = false;
-        anim = moleDoor.GetComponent<Animator>();
-
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-        Collider2D playerChecked = Physics2D.OverlapCircle(transform.position, 1.0f, LayerMask.GetMask("Player"));
-
-        if (playerChecked != null && gameManager.hasMoleId == true)
-        {
-            doorOpened = true;
-            Debug.Log("Open");
-            
-        }
-
-        if(playerChecked == null)
-        {
-            doorOpened = false;
-            Debug.Log("Close");
-        }
-        
-
-        anim.SetBool("Open", doorOpened);
+        anim.SetBool("Open",doorOpened);
     }
-
     void OpenCloseDoor()
     {
-        moleDoor.SetActive(!doorOpened);
+        this.gameObject.SetActive(!doorOpened);
     }
 }
---
+++ Assets/Ground/Brushes/MoleCity/MoleDoorCheck.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleDoorCheck : MonoBehaviour
+{
+    private GameManager gameManager;
+    [SerializeField] MoleDoor moleDoor;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindFirstObjectByType<GameManager>();
+        moleDoor.doorOpened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Collider2D playerChecked = Physics2D.OverlapCircle(transform.position, 1.0f, LayerMask.GetMask("Player"));
+
+        if (playerChecked != null && gameManager.hasMoleId == true)
+        {
+            moleDoor.doorOpened = true;
+            Debug.Log("Open");
+
+        }
+
+        if (playerChecked == null)
+        {
+            moleDoor.doorOpened = false;
+            Debug.Log("Close");
+        }
+    }
+}
