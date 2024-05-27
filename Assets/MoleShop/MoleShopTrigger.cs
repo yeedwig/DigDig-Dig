2bec1ccdf0f5b1a860088bfb048a794053ecb748
yeedwig <69492855+yeedwig@users.noreply.github.com>
2024-05-27 22:51:02 +0900

    MoleCity

--- Assets/MoleShop/MoleShopTrigger.cs
+++ Assets/MoleShop/MoleShopTrigger.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class MoleShopTrigger : MonoBehaviour
 {
     [SerializeField] ShopManager shopManager;
+
+    private void Start()
+    {
+        shopManager = FindFirstObjectByType<ShopManager>(); 
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
