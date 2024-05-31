75310d52a0d03d9eb5353c8e2d1d2b48e9309221
123minjae <moonlight6750@naver.com>
2024-05-31 22:57:59 +0900

    인벤토리 저장 마무리

--- Assets/SaveAndLoad/SaveLoadManager.cs
+++ Assets/SaveAndLoad/SaveLoadManager.cs
@@ -60,128 +60,18 @@ public class SaveLoadManager : MonoBehaviour
         {
             Load();
         }
-
-        //인벤토리 저장 테스트 공간
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            int saveIndex=0;
-            InventoryClass inventorySaveObject = new InventoryClass
-            {
-                item = new Item[0],
-                itemCount = new int[0],
-                durability = new float[0],
-                currentInventoryLevel = IM.currentInventoryLevel,
-                beltItem = new Item[0],
-                beltItemCount = new int[0],
-                beltDurability = new float[0]
-            };
-            for (int i = 0; i < IM.inventorySlotsLength; i++)
-            {
-                InventorySlot slot = IM.inventorySlots[i];
-                InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-
-                if(itemInSlot != null)
-                {
-                    Array.Resize(ref inventorySaveObject.item, saveIndex + 1);
-                    Array.Resize(ref inventorySaveObject.itemCount, saveIndex + 1);
-                    Array.Resize(ref inventorySaveObject.durability, saveIndex + 1);
-                    if (itemInSlot.item.isTool)
-                    {
-                        inventorySaveObject.durability[saveIndex] = itemInSlot.Durability;
-                    }
-                    inventorySaveObject.item[saveIndex] = itemInSlot.item;
-                    inventorySaveObject.itemCount[saveIndex++] = itemInSlot.count;
-                }
-            }
-            saveIndex = 0;
-            for(int i=0;i<6; i++)
-            {
-                InventorySlot slot = TM.ToolBeltInventory[i];
-                InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-                if(itemInSlot != null)
-                {
-                    Array.Resize(ref inventorySaveObject.beltItem, saveIndex + 1);
-                    Array.Resize(ref inventorySaveObject.beltDurability, saveIndex + 1);
-                    Array.Resize(ref inventorySaveObject.beltItemCount, saveIndex + 1);
-                    inventorySaveObject.beltItem[saveIndex] = itemInSlot.item;
-                    inventorySaveObject.beltDurability[saveIndex] = itemInSlot.Durability;
-                    inventorySaveObject.beltItemCount[saveIndex++] = itemInSlot.count;
-                }
-            }
-            string json = JsonUtility.ToJson(inventorySaveObject);
-            File.WriteAllText(SAVE_FOLDER + "/InventorySave.txt", json);
-        }
-        if (Input.GetKeyDown(KeyCode.F4))
-        {
-            if (File.Exists(SAVE_FOLDER + "/InventorySave.txt"))
-            {
-                string saveString = File.ReadAllText(SAVE_FOLDER + "/InventorySave.txt");
-                InventoryClass load = JsonUtility.FromJson<InventoryClass>(saveString);
-                IM.currentInventoryLevel = load.currentInventoryLevel;
-                IM.AddInventorySlots();
-                for(int i = 0; i < load.item.Length; i++)
-                {
-                    InventorySlot slot = IM.inventorySlots[i];
-                    IM.SpawnNewItem(load.item[i], slot);
-                    InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-                    if (load.item[i].stackable)
-                    {
-                        itemInSlot.count = load.itemCount[i];
-                        itemInSlot.RefreshCount();
-                    }
-                    if (load.item[i].isTool)
-                    {
-                        itemInSlot.Durability = load.durability[i];
-                    }
-                }
-                
-
-                for (int i =0; i< load.beltItem.Length; i++)
-                {
-                    InventorySlot slot = TM.ToolBeltInventory[i];
-                    InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-                    if (itemInSlot != null) { 
-                        Destroy(itemInSlot.gameObject);
-                    }
-                    IM.SpawnNewItem(load.beltItem[i], slot);
-                    itemInSlot = slot.GetComponentInChildren<InventoryItem>();
-
-                    if (load.beltItem[i].stackable)
-                    {
-                        itemInSlot.count = load.beltItemCount[i];
-                        itemInSlot.RefreshCount();
-                    }
-                    if (load.beltItem[i].isTool)
-                    {
-                        itemInSlot.Durability = load.beltDurability[i];
-                    }
-                }
-            }
-        }
     }
     private void Save()
     {
         SaveHealth.saveHealth(player.GetComponent<Health>());
         SaveGameManager.saveGameManager(gameManager.GetComponent<GameManager>());
+        SaveInventory.saveInventory(inventoryManager.GetComponent<InventoryManager>(),toolManager.GetComponent<ToolManager>());
     }
 
     private void Load()
     {
         SaveHealth.loadHealth(player.GetComponent<Health>(), healthBar.GetComponent<HealthBar>());
         SaveGameManager.loadGameManager(gameManager.GetComponent<GameManager>());
-    }
-
-    public class InventoryClass
-    {
-        //인벤
-        public Item[] item;
-        public int[] itemCount;
-        public float[] durability;
-        public int currentInventoryLevel;
-
-        //툴 벨트
-        public Item[] beltItem;
-        public int[] beltItemCount;
-        public float[] beltDurability;
+        SaveInventory.loadInventory(inventoryManager.GetComponent<InventoryManager>(), toolManager.GetComponent<ToolManager>());
     }
 }
