5f04de098c0ec2d118a25ee9a077abeab912d801
123minjae <moonlight6750@naver.com>
2024-06-12 14:30:22 +0900

    일단 물 테스트

--- Assets/Fluid/FluidManager.cs
+++ Assets/Fluid/FluidManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using static UnityEditor.PlayerSettings;
 
 public class FluidManager : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class FluidManager : MonoBehaviour
 
     // ¾×Ã¼ µñ¼Å³Ê¸® ¼±¾ð
     public Dictionary<Vector3Int, int> waterBlockDictionary; //ÁÂÇ¥¿Í ¼öÀ§¸¦ Ç¥½Ã
+    public Dictionary<Vector3Int, int> waterMapDictionary;
 
     //¾×Ã¼ tilebase ¹è¿­
     [SerializeField] TileBase[] waterTiles;
@@ -19,6 +21,8 @@ public class FluidManager : MonoBehaviour
     [SerializeField] float waterSpreadDelay;
     private float waterSpreadTimer = 0;
 
+    private int waterMask;
+
     //½Ì±ÛÅæ
     public static FluidManager instance = null;
     // Start is called before the first frame update
@@ -26,12 +30,14 @@ public class FluidManager : MonoBehaviour
     private void Awake()
     {
         waterBlockDictionary = new Dictionary<Vector3Int, int>();
+        waterMapDictionary = new Dictionary<Vector3Int, int>();
         instance = this;
     }
     void Start()
     {
         waterSpreadTimer = waterSpreadDelay;
         groundDictionary = groundDictionaryObject.GetComponent<GroundDictionary>();
+        waterMask = 1 << LayerMask.NameToLayer("Ground");
     }
 
     // Update is called once per frame
@@ -52,19 +58,53 @@ public class FluidManager : MonoBehaviour
 
     public void SpreadWater()
     {
-        
+        waterMapDictionary.Clear();
         foreach (KeyValuePair<Vector3Int, int> water in waterBlockDictionary)
         {
-            if (!waterBlockDictionary.ContainsKey(water.Key))
-            {
-                
-                waterBlockDictionary.Add(water.Key + new Vector3Int(0, -1, 0), 4);
-            }
-            
+            AddToWaterMap(water.Key, water.Value);
         }
-        foreach (KeyValuePair<Vector3Int, int> water in waterBlockDictionary)
+        ShowWater();
+    }
+
+    public void AddToWaterMap(Vector3Int pos, int level)
+    {
+        if(!waterMapDictionary.ContainsKey(pos)) {
+            waterMapDictionary.Add(pos, level);
+        }
+        else
+        {
+            waterMapDictionary[pos] = Mathf.Max(waterMapDictionary[pos],level);
+        }
+        
+        Collider2D waterCheck = Physics2D.OverlapCircle(((Vector2Int)pos) + new Vector2(0.5f, -0.5f), 0.4f, waterMask);
+        if (waterCheck == null)
+        {
+            AddToWaterMap(pos + new Vector3Int(0, -1, 0), 4);
+        }
+        
+        else if(waterCheck != null && level>0)
+        {
+            waterCheck = Physics2D.OverlapCircle(((Vector2Int)pos) + new Vector2(-0.5f, 0.5f), 0.4f, waterMask);
+            if (waterCheck==null)
+            {
+                AddToWaterMap(pos + new Vector3Int(-1,0, 0), level-1);
+            }
+            waterCheck = Physics2D.OverlapCircle(((Vector2Int)pos) + new Vector2(1.5f, 0.5f), 0.4f, waterMask);
+            if (waterCheck == null)
+            {
+                AddToWaterMap(pos + new Vector3Int(1, 0, 0), level - 1);
+            }
+        }
+        
+    }
+
+    public void ShowWater()
+    {
+        TilemapManager.instance.waterTilemap.ClearAllTiles();
+        foreach (KeyValuePair<Vector3Int, int> water in waterMapDictionary)
         {
             TilemapManager.instance.waterTilemap.SetTile(water.Key, waterTiles[water.Value]);
         }
     }
+    
 }
