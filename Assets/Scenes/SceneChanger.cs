bee797350970be9dd9c74d964f084de0b8a9c94a
yeedwig <69492855+yeedwig@users.noreply.github.com>
2024-06-13 20:30:51 +0900

    startSceneMoleMaking

--- Assets/Scenes/SceneChanger.cs
+++ Assets/Scenes/SceneChanger.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private GameObject CreditUI;
+    private bool creditOpen;
     void Start()
     {
         
@@ -22,5 +24,26 @@ public class SceneChanger : MonoBehaviour
     {
         SaveLoadManager.loaded = true;
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+
+    }
+
+    public void CreditButton()
+    {
+        if(creditOpen)
+        {
+            CreditUI.SetActive(false);
+            creditOpen = false;
+        }
+        else
+        {
+            CreditUI.SetActive(true);
+            creditOpen = true;
+        }
+
+       
     }
 }
---
+++ Assets/StartSceneUI/StartSceneMole.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneMole : MonoBehaviour
+{
+    [SerializeField] Transform[] moleSpawnPositions;
+    [SerializeField] GameObject startMole;
+    private Animator anim;
+
+    public float spawnTime;
+    [SerializeField] private float spawnTimer;
+    private bool isInCoroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = startMole.GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if(spawnTimer > spawnTime && !isInCoroutine)
+        {
+            
+            isInCoroutine = true;
+            startMole.transform.position = moleSpawnPositions[Random.Range(0,13)].position;
+            startMole.SetActive(true);
+            anim.SetTrigger("DigOut");
+            Debug.Log("Out");
+            StartCoroutine(MoleBehave());
+        }
+    }
+
+    IEnumerator MoleBehave()
+    {
+        Debug.Log("Idle");
+        anim.SetTrigger("Idle");
+        float waitSeconds = Random.Range(10.0f, 20.0f);
+        yield return new WaitForSeconds(waitSeconds);
+        anim.SetTrigger("DiggIng");
+        Debug.Log("DigIn");
+        //startMole.SetActive(false);
+        isInCoroutine = false;
+        spawnTimer = 0;
+    }
+
+}
